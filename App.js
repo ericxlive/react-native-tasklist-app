17be4508608f5e26b4c971092af7ede4df359656
Eric de Souza <ericdsky@gmail.com>
2018-03-22 02:00:13 -0400

    .eslintrc is a file to track errors and warnings in the code. Please install eslint plugin into your development tool in order to track the issues with the code.

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import Main from './app/components/Main';
+import Main from './src/components/Main';
 
 export default class App extends Component {
   render() {
